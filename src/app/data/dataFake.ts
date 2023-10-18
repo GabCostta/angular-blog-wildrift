905ee3c3e31b27a0d6a73be5cb08776dc98fbe83
Gabriel Costa <gabcostta37@gmail.com>
2023-10-18 00:36:58 -0300

    Criação e atualização do projeto

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,50 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://images.contentstack.io/v3/assets/blt370612131b6e0756/blt25c7cb03f8dbb71e/5f5a8d9769d060498b8e4c31/WR_meta_homepage.png",
+    "title": "BOAS-VINDAS AO LEAGUE OF LEGENDS: WILD RIFT",
+    "description": "Wild Rift é uma emocionante adaptação do League of Legends original, otimizada para oferecer uma experiência imersiva e cheia de ação em dispositivos móveis e consoles. Prepare-se para mergulhar em batalhas estratégicas, habilidades épicas e um cenário rico e evocativo. Wild Rift é uma emocionante adaptação do League of Legends original, otimizada para oferecer uma experiência imersiva e cheia de ação em dispositivos móveis e consoles. Prepare-se para mergulhar em batalhas estratégicas, habilidades épicas e um cenário rico e evocativo."
+  },
+  {
+    "id": "2",
+    "photo": "https://images.contentstack.io/v3/assets/blt370612131b6e0756/blt438a3298155d73a1/649245b474d5019930016411/Article_Art_Drop_Pose.jpg",
+    "title": "Baú de pose aleatória de outubro de 2023",
+    "description": "Adoce ainda mais suas vitórias! Comemore-as com um Baú de Pose Aleatória vinculando sua conta ao Prime Gaming."
+  },
+  {
+    "id": "3",
+    "photo": "https://images.contentstack.io/v3/assets/blt370612131b6e0756/blta3d7dd8c85017b8a/60c1189dff4b120facb60f1b/TORNEIOS_DE_COMUNIDADE_BANNER_SITE_WR_WIDE_CUT.jpg",
+    "title": "Torneios de comunidade - 14/10 a 20/10",
+    "description": "Participe dos torneios criados pela própria comunidade."
+  },
+  {
+    "id": "4",
+    "photo": "https://images.contentstack.io/v3/assets/blt370612131b6e0756/blt52d22e97becc178c/651b5b3f6e4311e7e5ed2650/WR-News-Website_Article_Bannner-News_WRY3_EKKO_450kb.jpg",
+    "title": "Comemoração do terceiro aniversário global do wild rift",
+    "description": "Estamos nos preparando para o nosso Aniversário Global e não vemos a hora de celebrarmos com você! Para iniciar a festança, nossa Galeria Pico de Poder já está disponível. Envie suas criações para habilitar recompensas de aniversário! Ah, e não deixe de conferir as outras festividades e brindes que preparamos!"
+  },
+  {
+    "id": "5",
+    "photo": "https://images.contentstack.io/v3/assets/blt370612131b6e0756/blt30b7ca1a1e406558/6524a4105ee09e2913aa43af/WR-News-Website_Article_Banner-News_Patch_notes_4.3d_Janna.jpg",
+    "title": "Notas da Atualização 4.3d do Wild Rift",
+    "description": "Esta é a última parte da Atualização Espírito de Luta! Temos um rework para Janna, balanceamentos e skins novinhas! Boas-vindas à Atualização 4.3d!"
+  },
+  {
+    "id": "6",
+    "photo": "https://interfaceingame.com/wp-content/uploads/league-of-legends-wild-rift/league-of-legends-wild-rift-amumu-champion-500x281.png",
+    "title": "Meu campeão Favorito: Amumu",
+    "description": "Amumu é um dos campeões disponíveis no League of Legends: Wild Rift, e é conhecido por ser um tanque e iniciador excepcional. Ele é um Yordle triste e solitário que possui habilidades temáticas de maldições e tristeza. Amumu é um excelente campeão para a linha de frente devido à sua capacidade de absorver dano e iniciar lutas com eficiência."
+  },
+  {
+    "id": "7",
+    "photo": "https://i.ytimg.com/vi/swz1QbJEgsI/maxresdefault.jpg",
+    "title": "Prévia da atualização 4.4",
+    "description": "Vamos comemorar a Atualização 4.4: Pico de Poder 2023! David e John estão celebrando três novos Campeões, reworks grandiosos, Passes Wild misteriosos e um modo de jogo inédito! Preparem os chapéus de festa para o dia 25 de outubro!"
+  },
+  {
+    "id": "5",
+    "photo": "https://images.contentstack.io/v3/assets/blt370612131b6e0756/blt60535337dbdbdea1/64c3562f0242c3576bbb7f3c/80223_WR_News_Website_Article_Banner.jpg",
+    "title": "Notas da Atualização 4.3a do Wild Rift",
+    "description": "VLADIMIR, O SANGUINÁRIO ESCARLATE - Um demônio sedento pelo sangue dos mortais, Vladimir tem influenciado os acontecimentos de Noxus desde os primórdios do império. Além de ter estendido sua vida por meios não naturais, sua maestria em hemomancia o permite controlar as mentes e os corpos de outras pessoas como se fossem parte de si. Nos exuberantes salões da aristocracia noxiana, isso possibilitou formar um culto fanático de personalidades ao seu redor, tudo isso para que ele possa fazer seus inimigos sangrarem nos becos escuros. Vladimir será lançado em 16 de agosto às 21:01 BRT."
+  },
+]
